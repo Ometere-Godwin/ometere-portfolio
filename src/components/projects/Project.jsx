756f2e0aa9d22ec18296e5def8599c8ff04d9e28
Ometere Oghene-Godwin <you@example.com>
2023-11-10 05:40:21 +0100

    editted

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -47,19 +47,16 @@ export default function Project() {
           github="https://github.com/Ometere-Godwin/nextjs_carhub_showcaseTere"
           vercel="https://nextjs-carhub-showcase-tere-8tiq.vercel.app/"
         />
-        {/* <ProjectCard
-          title="Youtube Clone"
-          description="This handy tool helps 
-          you create dummy text for all your 
-          layout needs. We are gradually 
-          adding new functionality and we 
-          welcome your suggestions and feedback"
-          img="assets/globe.png"
-          github="https://github.com/Ometere-Godwin/youtube_clone"
-          vercel=""
+        <ProjectCard
+          title="A Modern Website"
+          description="This is modern website built with some modern technologies. It show cases how to use a React Library called clsx
+          I also used technologies like Tailwing CSS, ReactJs, React Icons, Framer Motion."
+          img="assets/modernwensite.PNG"
+          github="https://github.com/Ometere-Godwin/modernwebsite"
+          vercel="https://vercel.com/ometere-godwin/modernwebsite"
         />
 
-        <ProjectCard
+        {/* <ProjectCard
           title="Youtube Clone"
           description="This handy tool helps 
           you create dummy text for all your 
